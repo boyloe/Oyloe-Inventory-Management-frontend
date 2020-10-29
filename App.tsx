07f3eb300daad2ba903e0f45b8cd32b178ca5190
boyloe <boyloe@gmail.com>
2020-10-29 17:05:47 -0600

    switch to functional app component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Product } from './types'
+import ProductCard from './components/ProductCard';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import HomeScreen from './components/HomeScreen'
+
+const baseURL = "http://localhost:4000"
+export interface IAppProps {
+  products: Product[]
+}
+const Stack = createStackNavigator()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+  const [products, setProducts] = useState([])
+
+
+  useEffect(() => {
+    fetch(`${baseURL}/products`)
+      .then(response => response.json())
+      .then(products => setProducts(products))
+  }, [])
+
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+
+
+  }
+
+  // displayProducts = () => this.state.products.map(product => <ProductCard key={product.id} product={product} />)
 
 const styles = StyleSheet.create({
   container: {
