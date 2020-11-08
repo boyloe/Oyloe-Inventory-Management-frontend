776b91916e3c0fc487a46804283c1623459ff156
boyloe <boyloe@gmail.com>
2020-11-07 22:09:01 -0700

    Add Card and List to for Delivery Ticket

--- components/DeliveryFormScreen.tsx
+++ components/DeliveryFormScreen.tsx
@@ -17,7 +17,7 @@ export interface HomeScreenProps {
 
 export const DeliveryFormScreen:React.FC<HomeScreenProps> = ({navigation, products}) => {
 
-    const [selectedProduct, setSelectedProduct] = useState('')
+    const [selectedProduct, setSelectedProduct] = useState('CyberMul')
     const [quantity, setQuantity] = useState('')
 
 
@@ -32,7 +32,11 @@ export const DeliveryFormScreen:React.FC<HomeScreenProps> = ({navigation, produc
 
     const getProductOptions = (products:Product[]) => {
         return products.map(product => {
-            return <Picker.Item label={product.name} value={product.name} key={product._id} />
+            return <Picker.Item 
+                    label={product.name} 
+                    value={product.name} 
+                    color={brownPalette.brown10}
+                    key={product._id} />
         })
     }
 
@@ -45,27 +49,32 @@ export const DeliveryFormScreen:React.FC<HomeScreenProps> = ({navigation, produc
                 <View style={styles.pickerContainer}>
                     <Picker 
                         selectedValue={selectedProduct}
-                        style={{height: 80, width: 200}}
-                        itemStyle={{height: 150, borderWidth:2, backgroundColor: brownPalette.brown3, borderRadius: 12}}
+                        style={styles.pickerStyle}
+                        itemStyle={styles.pickerItemStyle}
                         onValueChange={(value, key) => {
                             setSelectedProduct(value)
                         }}
-                    >
+                        
+                        
+                    >   
                         {getProductOptions(products)}
                     </Picker>
-                </View>      
-                <TextInput 
-                    style={styles.textBox} 
-                    onChangeText={text => setQuantity(text)} >
-                </TextInput>
-                <Button 
-                    buttonStyle={styles.buttonStyle}
-                    titleStyle={styles.buttonTitleStyle}
-                    title='Add Product to Ticket'
-                    onPress={addProductToDelivery}
-                >
-                    
-                </Button>
+                </View>  
+                <View style={styles.textInputContainer}>
+                    <TextInput 
+                        style={styles.textBox} 
+                        onChangeText={text => setQuantity(text)} >
+                    </TextInput>
+                </View> 
+                <View style={styles.buttonContainer}>
+                    <Button 
+                        buttonStyle={styles.buttonStyle}
+                        titleStyle={styles.buttonTitleStyle}
+                        title='Add Product to Ticket'
+                        onPress={addProductToDelivery}
+                    />                  
+                </View>   
+                
             </View>
                 
                 
@@ -76,19 +85,27 @@ export const DeliveryFormScreen:React.FC<HomeScreenProps> = ({navigation, produc
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: brownPalette.brown1,
+        backgroundColor: '#faf5ef',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    textBox: {
+    textInputContainer: {
         flex:1,
-        borderStyle: 'solid',
+        width: 400,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textBox: {
         borderRadius: 8,
-        borderColor: brownPalette.brown8,
-        borderWidth: 5,
-        width: '50%',
+        backgroundColor: brownPalette.brown3,
+        width: '80%',
         padding: 10,
         margin: 10
+    },
+    buttonContainer: {
+        flex:1,
+        justifyContent: 'flex-end',
+        marginBottom: 30
     },
     buttonTitleStyle: {
         fontFamily: 'Futura', 
@@ -96,13 +113,25 @@ const styles = StyleSheet.create({
         fontWeight: '700'
     },
     buttonStyle: {
-        backgroundColor: brownPalette.brownBase,
-        width: 240,  
+        backgroundColor: brownPalette.brownBase,  
         paddingVertical: 10,
+        width: 300,
         margin:10,
         borderRadius: 7
     },
     pickerContainer:{
-        flex:1
+        flex:1,
+        justifyContent: 'center'
+    },
+    pickerStyle: {
+        height: 50, 
+        width: 200,        
+    },
+    pickerItemStyle: {
+        height: 120, 
+        width: 180,
+        backgroundColor: '#f0e3d0', 
+        borderRadius: 12,       
+
     }
 });
--- components/DeliveryScreen.tsx
+++ components/DeliveryScreen.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
-import { Button, Divider } from 'react-native-elements'
+import { Button, Card, ListItem } from 'react-native-elements'
 import {  NavigationScreenProp } from 'react-navigation'
 import { Product } from '../types'
 import { useState, useEffect } from 'react'
 import { brownPalette } from '../assets/ColorPalette'
+
 
 const baseURL = "https://oyloe-inventory-management.herokuapp.com"
 
@@ -22,6 +23,7 @@ const DeliveryScreen:React.FC<DeliveryScreenProps> = ({navigation,route}) => {
     }])
     let params = route.params
     useEffect(() => {
+        console.log({params})
         if (params != undefined || params != route.params) {
             const {product} = route.params
             setProductsDelivered([...productsDelivered, {name: product.name, quantity: product.quantity}])
@@ -31,15 +33,15 @@ const DeliveryScreen:React.FC<DeliveryScreenProps> = ({navigation,route}) => {
     }, [route.params])
 
     const displayProductsDelivered = () => {
-        return productsDelivered.map((product,index) => (
-            <View style={{flexDirection: 'row'}} key={index}>
-                <Text>Name: {product.name}</Text>
-                <Text>Quantity: {product.quantity}</Text>
-            </View>
-        )
-
-                
-        )
+        return productsDelivered.map((product,index) => {
+                // return <Text key={index}>{product.name}  {product.quantity}</Text>
+                return <ListItem key={index} containerStyle={{backgroundColor:brownPalette.brown1}} bottomDivider={true}>
+                        <ListItem.Content>
+                            <ListItem.Title style={{fontSize: 30}}>{product.name}</ListItem.Title>
+                            <ListItem.Subtitle style={{fontStyle: 'italic'}}>Quantity: {product.quantity}</ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+        })
     }
     //submits to delivery collection BE, updates Inventory collection BE,  and redirects to home}
     const handleDeliverySubmit = () => {
@@ -53,23 +55,24 @@ const DeliveryScreen:React.FC<DeliveryScreenProps> = ({navigation,route}) => {
     }
     return(
         <View style={styles.container}>
-            <View style={styles.productsDeliveredContainer}>
-                {displayProductsDelivered()}
+            <View style={{flex:1}}>
+                <Card containerStyle={{padding:0, width: 400}}>
+                    {displayProductsDelivered()}       
+                </Card>
             </View>
-            <View style={styles.buttonContainer}>
-                <Button 
-                    buttonStyle={styles.buttonStyle}
-                    title="Add Product to Ticket"
-                    titleStyle={styles.titleStyle}
-                    onPress={() => navigation.navigate('DeliveryForm')}
-                />   
-                <Button 
-                    buttonStyle={styles.buttonStyle}
-                    titleStyle={styles.titleStyle}
-                    title="Submit Delivery Ticket"
-                    onPress={handleDeliverySubmit}  
-                />   
-            </View>
+            <Button 
+                buttonStyle={styles.buttons}
+                title="Add Product to Ticket"
+                titleStyle={{fontFamily: 'Futura', color: brownPalette.brown10}}
+                onPress={() => navigation.navigate('DeliveryForm')}
+            />    
+            <Button 
+                buttonStyle={styles.buttons}
+                titleStyle={{fontFamily: 'Futura', color: brownPalette.brown10}}
+                title="Submit Delivery Ticket"
+                onPress={handleDeliverySubmit}  
+            />    
+
         </View>
     )
 
@@ -80,33 +83,24 @@ const styles= StyleSheet.create({
     container: {
         flex: 1, 
         alignItems: 'center', 
-        justifyContent:'flex-end',
-        backgroundColor: brownPalette.brown1,
-        paddingVertical: 20
+        justifyContent:'center',
+        backgroundColor: brownPalette.brown1
 
     },
-    productsDeliveredContainer: {
-        flex: 3,
-        marginVertical:20,
+    titleText: {
+        fontSize: 28, 
+        textAlign: 'center',
+        flex: 1,
+        padding: 20
+
+
     },
-    buttonStyle: {
+    buttons: {
         backgroundColor: brownPalette.brownBase,
-        width: 240,  
-        paddingVertical: 10,
-        margin:10,
-        borderRadius: 7
-    },
-    buttonContainer: {
-        flex:1,
-
-
-        justifyContent: 'flex-end',
-        flexDirection: 'column'
-    },
-    titleStyle: {
-        fontFamily: 'Futura', 
-        color: brownPalette.brown10
-    }
+        borderRadius: 12,
+        width: 200, 
+        padding:4, 
+        margin:10}
 })
     
 export default DeliveryScreen
